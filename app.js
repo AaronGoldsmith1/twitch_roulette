e4860be9dcf88205380080e158d98c19b3acbe8e
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-01-29 15:14:37 -0800

    get access token

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const clientSecret = 'khcxdmodyqxoajyybl0mguqzmqjb6m';
 
 const tokenUrl = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
 
+let access_token;
+
 // {
 //     "access_token": "g62m6nffxtjyglauttx17u3j286ara",
 //     "expires_in": 5547325,
@@ -10,4 +12,19 @@ const tokenUrl = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client
 // }
 
 
-const topStreamsUrl = 'https://api.twitch.tv/helix/streams';
+const topStreamsUrl = 'https://api.twitch.tv/helix/streams?first=100';
+
+function makeRequest(url, requestMethod, requestHeaders = {}) {
+  
+ }
+
+function getAccessToken() {
+  const request = new Request(tokenUrl, { method: 'POST' });
+
+  fetch(request).then((response) => response.json())
+    .then((responseJson) => { 
+      access_token = responseJson.access_token;
+    }).catch((error) => { 
+      console.error(error);
+    });
+}
