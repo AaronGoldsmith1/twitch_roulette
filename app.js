680f3762d385b1b07abf283e1d917fb8a16337a1
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2021-01-29 16:38:39 -0800

    embed working

--- app.js
+++ app.js
@@ -2,22 +2,10 @@ const clientId = 'usg4v0i9m8c8ow94fj7w1w8jrywo9k';
 const clientSecret = 'khcxdmodyqxoajyybl0mguqzmqjb6m';
 
 const tokenUrl = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
+const topStreamsUrl = 'https://api.twitch.tv/helix/streams?first=100';
 
 let access_token = 'xo0t4ahhetbujy7qvyx5jjf078e00p'
 let streams;
-
-// {
-//     "access_token": "g62m6nffxtjyglauttx17u3j286ara",
-//     "expires_in": 5547325,
-//     "token_type": "bearer"
-// }
-
-
-const topStreamsUrl = 'https://api.twitch.tv/helix/streams?first=100';
-
-function makeRequest(url, requestMethod, requestHeaders = {}) {
-  
- }
 
 function getAccessToken() {
   const request = new Request(tokenUrl, { method: 'POST' });
@@ -48,7 +36,7 @@ function getTopStreams() {
     });
 }
 
-function getRandomStreamUrl(streams) {
+function getRandomStream(streams) {
   let randomStream = streams[Math.floor(Math.random()*streams.length)];
   return randomStream.user_name;
 }
@@ -57,7 +45,7 @@ function embedStream() {
   new Twitch.Embed("twitch-embed", {
     width: 800,
     height: 500,
-    channel: "dakotaz",
+    channel: 'dakotaz',
     parent: ['localhost']
   });
 }
